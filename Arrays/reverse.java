3efaf5196997e512b89762ec71df388f44090921
Shivam Jha <shivamjhaa@gmail.com>
2025-12-14 12:06:03 +0530

    Reversing a string

---
+++ Arrays/reverse.java
@@ -0,0 +1,12 @@
+class reverse
+{
+    public static void main(String[]args)
+    {
+        String name = "Shivam";
+
+        StringBuffer sb = new StringBuffer(name);
+        sb.reverse();
+        System.out.println(sb);
+    
+    }
+}
